abb9c6da53ba46371ef4b959fde37fe364cc5297
tamagokakepiza <nyororo89@gmail.com>
2022-05-21 00:42:32 +0900

    セクション5-8

--- sample.c
+++ sample.c
@@ -1,13 +1,6 @@
 #include <stdio.h>
 
 int main (void) {
-  int apple = 10;
-  printf("%d\n", apple);
-  apple ++;
-  printf("%d\n", apple);
-  apple ++;
-  printf("%d\n", apple);
-  apple --;
-  printf("%d\n", apple);
+  printf("%d", (int) (3.14*3*3));
   return 0;
 }
