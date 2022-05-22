d6e4d71f4bc31a3000119c5ff88903f3561947ec
tamagokakepiza <nyororo89@gmail.com>
2022-05-22 19:41:04 +0900

    セクション7-5

--- sample.c
+++ sample.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
 int main (void) {
-  int i;
-  for(i = 1; i <= 10; i++){
-    printf("%d 回目:ありがとう\n", i);
+  int year = 1;
+  double money = 1000000;
+  while (money < 100000000) {
+    year++;
+    money = (money + 600000) * 1.05;
   }
+  printf("%d 年目: %f 円となり、資産が1億円を超えます", year, money);
   return 0;
 }
