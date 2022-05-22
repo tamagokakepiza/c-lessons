3d7e8296bcc1dcd695316ba8e5812fb1b84f90c3
tamagokakepiza <nyororo89@gmail.com>
2022-05-22 19:25:14 +0900

    セクション7-3

--- sample.c
+++ sample.c
@@ -1,24 +1,9 @@
 #include <stdio.h>
 
 int main (void) {
-  int number;
-  scanf("%d" ,&number);
-  switch (number){
-  case 1:
-    printf("ハリー");
-    break;
-  case 2:
-    printf("ロン");
-    break;
-  case 3:
-    printf("ハーマイオニー");
-    break;
-  case 4:
-    printf("マルファイ");
-    break;
-  default:
-    printf("正しい番号を入力してください");
-    break;
+  int i;
+  for(i = 1; i <= 10; i++){
+    printf("%d 回目:ありがとう\n", i);
   }
   return 0;
 }
