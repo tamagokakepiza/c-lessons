e29b349a1edf390123f0ca459488e20e74324158
tamagokakepiza <nyororo89@gmail.com>
2022-05-22 18:20:06 +0900

    セクション6-8

--- sample.c
+++ sample.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
 int main (void) {
-  int number;
-  scanf ("%d", &number);
-  if (number == 10)
-    printf("10と同じです");
-  else
-    printf("10ではありません");
+  int age;
+  scanf("%d" ,&age);
+  if (age <=3) {
+    printf("0円です");
+  } else if (age <= 20) {
+    printf("1000円です");
+  } else {
+    printf("2000円です");
+  }
   return 0;
 }
