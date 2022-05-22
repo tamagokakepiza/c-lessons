488f1741397ca3723d09bca6b8ddfe661a3b102d
tamagokakepiza <nyororo89@gmail.com>
2022-05-23 02:00:42 +0900

    セクション8-3

--- sample.c
+++ sample.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 
 int main (void) {
-  int i = 10;
-  while (i < 5){
-    printf("while文です\n");
-  }
-  do{
-    printf("do while文です\n");
-  }while (i < 5);
+  return 0;
+}
+
+int thankyou (void) {
+  printf("ありがとう");
   return 0;
 }
