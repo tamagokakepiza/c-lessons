bfb26897b17b5945754ed0908eb6ce8fa9c25c80
tamagokakepiza <nyororo89@gmail.com>
2022-05-31 21:12:13 +0900

    セクション9-3

--- sample.c
+++ sample.c
@@ -1,18 +1,8 @@
 #include <stdio.h>
-int fruits(int apple);
 int main (void) {
-  int hoge;
-  hoge = fruits(5);
-  if (hoge < 3000){
-    printf("予算に収まりました");
-  } else {
-    printf("予算オーバーです");
-  }
+  
+    int apple = 30;
+  
+  printf("%d", apple);
   return 0;
-}
-
-int fruits(int apple){
-  int money;
-  money = apple * 300;
-  return money;
 }
