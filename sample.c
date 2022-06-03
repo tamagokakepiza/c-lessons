f1039e7e1a033c28b841ba740e7117743ab9b9ea
tamagokakepiza <nyororo89@gmail.com>
2022-06-03 23:59:05 +0900

    セクション10-5

--- sample.c
+++ sample.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 int main (void) {
-  int array [5] = {12,13,14};
-  printf("%d\n", array[0]);
-  printf("%d\n", array[1]);
-  printf("%d\n", array[2]);
-  printf("%d\n", array[3]);
-  printf("%d\n", array[4]);
+  int array [] = {12,13,14};
+  printf("%d", array[1]);
   return 0;
 }
