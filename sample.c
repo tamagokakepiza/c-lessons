e8af919112232ad190d8bd58acfcecfd4a4e5eea
tamagokakepiza <nyororo89@gmail.com>
2022-06-09 18:53:21 +0900

    セクション11-2

--- sample.c
+++ sample.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 int main (void) {
-  int array [5] = {12,13,14};
-  int i;
-  for(i=0; i< sizeof(array) / sizeof(array[0]); i++){
-  printf("%d番目:%d\n",i, array[i]);
-  }
+  char c = 'A';
+  printf("%c", c);
   return 0;
 }
