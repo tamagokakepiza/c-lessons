4be37e72bc8dcb8c3fdee541c55354c759334552
tamagokakepiza <nyororo89@gmail.com>
2022-06-11 15:26:07 +0900

    セクション11-4

--- sample.c
+++ sample.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 int main (void) {
   char str [5] = {'J','O','H','N','\0'};
-  printf("%s", str);
+  printf("%s\n", str);
+  char str2 [] = "JOHN";
+  printf("%s\n", str2);
   return 0;
 }
