85e43eaa1fa1c0f3c2782ff31f24ba24044e575b
tamagokakepiza <nyororo89@gmail.com>
2022-06-19 01:43:13 +0900

    セクション12-10

--- sample.c
+++ sample.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 
+void culc(int x, int y, int *a1, int *a2);
+
 int main (void) {
-  int *p, i;
-  p = &i;
-  *p = 15;
-  printf("%d\n", *p);
-  printf("%d\n", i);
+  int plus;
+  int minus;
+  culc(30, 10, &plus, &minus);
+  printf("%d\n", plus);
+  printf("%d\n", minus);
   return 0;
+}
+
+void culc(int x, int y,int *a1, int *a2){
+  *a1 = x + y;
+  *a2 = x - y;
 }
